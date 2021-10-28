d81e3e32ab261073ea6bf31a062eb38f3e207b01
yjy1014 <jiyeong3@daum.net>
2021-10-28 17:26:20 +0900

    (염지영) 봉사게시판 달력 작업 10.28 17:26
    volunteerBoardList.jsp - 달력 조회 구현
    blank-volunteer-640.jpg 이미지 파일 추가
    VolunteerBoardViewServlet, volunteerBoardView.jsp 생성
    summernote-bs4.css, summernote-bs4.js, summernote-ko-KR.js,
    summernote폰트추가

--- src/com/petdaon/mvc/volunteer_board/controller/VolunteerBoardListServlet.java
+++ src/com/petdaon/mvc/volunteer_board/controller/VolunteerBoardListServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.petdaon.mvc.volunteer_board.model.service.VolunteerBoardService;
 import com.petdaon.mvc.volunteer_board.model.vo.VolunteerBoard;
 
@@ -24,15 +25,26 @@ public class VolunteerBoardListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request
+			.getRequestDispatcher("/WEB-INF/views/volunteer_board/volunteerBoardList.jsp")
+			.forward(request, response);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 업무로직
 		List<VolunteerBoard> list = volunteerBoardService.selectVolunteerBoardList();
 		//System.out.println("list@VolunteerBoardListServlet = " + list);
 		
 		// view단 처리
-		request.setAttribute("list", list);
-		request
-			.getRequestDispatcher("/WEB-INF/views/volunteer_board/volunteerBoardList.jsp")
-			.forward(request, response);
+		//request.setAttribute("list", list);
+		
+		response.setContentType("application/json; charset=utf-8"); // json이라는 것을 헤더에 명시한다.
+
+		new Gson().toJson(list, response.getWriter());
 	}
+	
 
 }
