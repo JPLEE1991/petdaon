0e396166a11fbd87530d330e525d01e4d734754c
over9 <over9@DESKTOP-FBIRCVG>
2021-11-04 15:31:56 +0900

    (김경태) 21.11.04 15:26
    adopt.jsp
    adoptboard.java
    adoptWriting.jsp
    수정및 추가

---
+++ src/com/petdaon/mvc/adopt_board/controller/adoptboard.java
@@ -0,0 +1,27 @@
+package com.petdaon.mvc.adopt_board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class adoptborad
+ */
+@WebServlet("/adopt_board/adoptForm")
+public class adoptboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//view단 처리
+        request
+        .getRequestDispatcher("/WEB-INF/views/adopt_board/adoptForm.jsp")
+        .forward(request, response);
+	}
+
+	
+
+}
